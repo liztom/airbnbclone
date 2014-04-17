748a018f3e3e84671805ee028614f44dd3f8da43
Chris Lind & liz tom <student@epicodus.com>
2014-04-17 09:28:21 -0700

    create index page and route users to correct index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Bearbnb::Application.routes.draw do
   devise_for :users
+
   resources :users do
     resources :listings do
       resources :photos
     end
   end
 
+
+
   resources :amenities do
     resources :tags
   end
-  root to: 'users#index'
+  root to: 'listings#index'
 end
