2df9d8eb27d34359de2b076bb7d2dfaebac33d12
Chris Lind & liz tom <student@epicodus.com>
2014-04-18 16:56:18 -0700

    styles the pages, adds tabs for listing layout

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -11,5 +11,12 @@ $(document).ready(function() {
     })
   };
 
+  $(".fancybox").fancybox({
+    'transitionIn'  : 'elastic',
+    'transitionOut' : 'elastic',
+    'speedIn'   : 600,
+    'speedOut'    : 200,
+    'overlayShow' : false
+  });
 
 })
